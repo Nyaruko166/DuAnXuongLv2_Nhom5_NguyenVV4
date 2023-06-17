975b26ba18cd1603092f54eee9efce8302e1b5db
Nyaruko <occho1666@gmail.com>
2023-06-17 18:01:53 +0700

    Init Project

---
+++ src/main/java/com/example/duanxuonglv2_nhom5_nguyenvv4/DuAnXuongLv2Nhom5NguyenVv4Application.java
@@ -0,0 +1,13 @@
+package com.example.duanxuonglv2_nhom5_nguyenvv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnXuongLv2Nhom5NguyenVv4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuAnXuongLv2Nhom5NguyenVv4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/duanxuonglv2_nhom5_nguyenvv4/DuAnXuongLv2Nhom5NguyenVv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duanxuonglv2_nhom5_nguyenvv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnXuongLv2Nhom5NguyenVv4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
