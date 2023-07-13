5c818026248bae66de903eabf77667a571c15580
Nyaruko166 <occho1666@gmail.com>
2023-07-13 19:25:43 +0700

    Added template, Done HoaDon View
    Signed-off-by: Nyaruko166 <occho1666@gmail.com>

---
+++ src/main/webapp/resource/plugins/bower_components/gmaps/jquery.gmaps.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Nyaruko.
+ */
+
+$(document).ready(function() {
+    // Simple map
+    map = new GMaps({
+        el: '#gmaps-simple',
+        lat: 34.05,
+        lng: -78.72,
+        zoom: 5,
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        overviewMapControl: false
+    });
+});
